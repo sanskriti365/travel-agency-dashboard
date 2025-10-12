aa93b8f043777c452a5f1cdfb436563e8ab0adc9
Sanskriti Sanskriti <Sanskriti.Sanskriti@ibm.com>
2025-10-12 23:44:53 +0530

    Routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminLayout = () => {
+    return (
+        <div className='admin-layout'>
+         AdminLayout   
+        </div>
+    );
+}
+
+export default AdminLayout ;
---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllUsers = () => {
+    return (
+        <div>
+            allusers
+        </div>
+    );
+}
+
+export default AllUsers;
